9d647be206acb8aa9e5d056034c7fd03de6b0fef
Abhinav-Saxena-dev <abhinavsaxena148@gmail.com>
2023-01-01 16:46:22 +0530

    added book illustration of about us page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import LandingPage from './Pages/Landing-page/landing-page.component';
 import Header from './Components/Header/header.component';
+import AboutUs from './Pages/About-us/aboutus.page';
 
 function App() {
   return (
     <div className="select-none">
       <Header/>
       <LandingPage/>
+      <AboutUs/>
     </div>
   );
 }
